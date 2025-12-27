2381febe1807bc5fc4a9f6eab4b1bdba3a878ffd
VeregaS <sergeiusenkov56@gmail.com>
2025-12-27 09:24:06 +0300

    shared

--- shared/src/commonMain/kotlin/ru/recipeapp/models/AuthModels.kt
+++ shared/src/commonMain/kotlin/ru/recipeapp/models/AuthModels.kt
@@ -3,12 +3,6 @@ package ru.recipeapp.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LoginRequest(
-    val email: String,
-    val password: String
-)
-
-@Serializable
 data class AuthResponse(
     val token: String,
     val userEmail: String
--- shared/src/commonMain/kotlin/ru/recipeapp/models/Recipe.kt
+++ shared/src/commonMain/kotlin/ru/recipeapp/models/Recipe.kt
@@ -4,9 +4,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Recipe(
-    val id: Int,
+    val id: Int = 0,
     val title: String,
     val ingredients: String,
     val description: String,
-    val authorLogin: String
+    val authorLogin: String,
+    val imageUrl: String? = null,
+    val tagId: Int? = null
 )
